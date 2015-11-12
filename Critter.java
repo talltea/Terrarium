75bb67ce7549b5d8c693f8c182948e9a980df694
TallTea <kuzovleva@gmail.com>
2015-11-11 18:16:58 -0800

    Critter class file created

---
+++ Critter.java
@@ -0,0 +1,23 @@
+// Critter.java
+
+/*
+ * NaCoWriMo: Project 1
+ * Start: Nov 5, 2015
+ * End: -----
+ * LoC: ??
+
+ * Terrarium presents the visual game board and
+ * handles the GUI and animation. 
+ * The TMap class handles the game logic, 
+ * while the Critter class handles information
+ * about the enhabitants in the game.
+ */
+
+public class Critter {
+    private int health;
+
+    public Critter() {
+	System.out.println("You've made a new critter!");
+    }
+
+}
